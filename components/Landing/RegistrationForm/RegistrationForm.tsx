e4186fb87762a5d3dff90ca1b6e7cf6491b4cdb9
Saba-Var <vartasashvili94@gmail.com>
2022-07-27 11:57:22 +0400

    chore: create schema folder for yup schemas

--- components/Landing/RegistrationForm/RegistrationForm.tsx
+++ components/Landing/RegistrationForm/RegistrationForm.tsx
@@ -1,42 +1,23 @@
+import { registrationFormValidationSchema } from 'schemas'
 import { AuthInputField, Button } from 'components'
 import { useTranslation } from 'next-i18next'
 import { Formik, Form } from 'formik'
-import * as Yup from 'yup'
 
 const RegistrationForm = () => {
   const { t } = useTranslation()
-  const validate = Yup.object({
-    name: Yup.string()
-      .required('name-required')
-      .min(3, 'name-min')
-      .max(15, 'max-char')
-      .matches(/^[a-z0-9]+$/g, 'lower-required'),
-
-    email: Yup.string().required('email-required').email('valid-email'),
-
-    password: Yup.string()
-      .required('password-required')
-      .min(8, 'password-min')
-      .max(15, 'max-char')
-      .matches(/^[a-z0-9]+$/g, 'lower-required'),
-
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'password-match')
-      .required('confirmPassword-required'),
-  })
 
   return (
     <Formik
+      validationSchema={registrationFormValidationSchema}
+      onSubmit={() => {}}
       initialValues={{
         confirmPassword: '',
         password: '',
         email: '',
         name: '',
       }}
-      validationSchema={validate}
-      onSubmit={() => {}}
     >
-      {(formik) => {
+      {() => {
         return (
           <Form className='mt-6 flex flex-col justify-center items-center animate-fade-in'>
             <div className='flex flex-col gap-4 mb-8'>
