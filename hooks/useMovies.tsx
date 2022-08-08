a93088987c37ad658407cb9a4d7e292922d32fdc
Saba-Var <vartasashvili94@gmail.com>
2022-08-08 20:02:00 +0400

    feat: fetch added movies by current user

--- hooks/useMovies.tsx
+++ hooks/useMovies.tsx
@@ -1,10 +1,31 @@
 import { useTranslation } from 'next-i18next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNewsFeed } from './useNewsFeed'
+import { getAllMovies } from 'services'
+import { AllMovie } from 'types'
 
 export const useMovies = () => {
   const { t } = useTranslation()
 
   const [showAddMovieForm, setShowAddMovieForm] = useState(false)
 
-  return { t, showAddMovieForm, setShowAddMovieForm }
+  const [movieList, setMovieList] = useState<AllMovie>([])
+
+  const { userData } = useNewsFeed()
+
+  useEffect(() => {
+    const fetchAllMovies = async () => {
+      try {
+        if (userData._id) {
+          const response = await getAllMovies(userData._id)
+          setMovieList(response.data)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchAllMovies()
+  }, [userData._id])
+
+  return { t, showAddMovieForm, setShowAddMovieForm, movieList }
 }
